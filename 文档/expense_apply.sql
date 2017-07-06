e99db7e0c62f3944fbb30867b9673eae7341ffd8
KeSJ <649206599@qq.com>
2017-07-06 08:48:39 +0800

    数据库表结构
    更改业务流程后

--- 文档/expense_apply.sql
+++ 文档/expense_apply.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50715
 File Encoding         : 65001
 
-Date: 2017-07-05 14:49:04
+Date: 2017-07-06 08:45:34
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,12 +24,28 @@ CREATE TABLE `accept` (
   `TId` varchar(10) NOT NULL,
   `AcceptSum` double NOT NULL,
   `ApplyId` int(11) NOT NULL,
-  `ApplyType` binary(1) NOT NULL,
+  `ApplyType` int(11) NOT NULL,
   PRIMARY KEY (`AcceptId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of accept
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `appendix`
+-- ----------------------------
+DROP TABLE IF EXISTS `appendix`;
+CREATE TABLE `appendix` (
+  `AppendixId` int(11) NOT NULL,
+  `ApplyId` int(11) NOT NULL,
+  `ApplyType` int(11) NOT NULL,
+  `FilePath` varchar(256) NOT NULL,
+  PRIMARY KEY (`AppendixId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of appendix
 -- ----------------------------
 
 -- ----------------------------
@@ -46,6 +62,12 @@ CREATE TABLE `budget` (
   `ProFinishTime` date NOT NULL,
   `SelfRaised` double NOT NULL,
   `Apply` double NOT NULL,
+  `BudAppTea` varchar(10) NOT NULL,
+  `BudAppDate` date NOT NULL,
+  `BudCheck` varchar(10) DEFAULT NULL,
+  `BudCheckDate` date DEFAULT NULL,
+  `BudResult` tinyint(1) DEFAULT NULL,
+  `BudRejectReason` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`BudId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -102,16 +124,18 @@ INSERT INTO `budgetitem` VALUES ('12', '专家咨询费', '1');
 DROP TABLE IF EXISTS `business`;
 CREATE TABLE `business` (
   `BId` int(11) NOT NULL AUTO_INCREMENT,
-  `BudId` int(11) DEFAULT NULL,
+  `BudId` int(11) NOT NULL,
   `BUnit` varchar(50) NOT NULL,
-  `BTeaName` varchar(20) NOT NULL,
-  `BTeaPos` varchar(10) DEFAULT NULL,
   `BReason` varchar(30) NOT NULL,
   `BAppend` int(11) NOT NULL,
   `BSum` double NOT NULL,
-  `BFinStaffId` varchar(10) NOT NULL,
+  `BAppTea` varchar(10) NOT NULL,
+  `BAppDate` datetime NOT NULL,
   `BAgent` varchar(10) DEFAULT NULL,
-  `BDate` date NOT NULL,
+  `BCheckStaff` varchar(10) DEFAULT NULL,
+  `BCheckDate` datetime DEFAULT NULL,
+  `BRejectReason` varchar(256) DEFAULT NULL,
+  `BResult` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`BId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -199,10 +223,13 @@ CREATE TABLE `expense` (
   `EId` int(11) NOT NULL AUTO_INCREMENT,
   `BudId` int(11) NOT NULL,
   `EAppend` int(11) NOT NULL,
-  `EManager` varchar(10) DEFAULT NULL,
+  `EAppTea` varchar(10) NOT NULL,
+  `EAppDate` datetime NOT NULL,
   `EAgent` varchar(20) DEFAULT NULL,
-  `EPhone` varchar(15) DEFAULT NULL,
-  `EDate` date NOT NULL,
+  `ECheckStaff` varchar(10) DEFAULT NULL,
+  `ECheckDate` datetime DEFAULT NULL,
+  `EResult` tinyint(1) DEFAULT NULL,
+  `ERejectReason` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`EId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -255,6 +282,8 @@ CREATE TABLE `financialstaff` (
   `FSId` varchar(10) NOT NULL,
   `FSName` varchar(20) NOT NULL,
   `FSPwd` varchar(20) NOT NULL,
+  `FSCreateDate` datetime NOT NULL,
+  `FSStopDate` datetime DEFAULT NULL,
   PRIMARY KEY (`FSId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -269,58 +298,59 @@ DROP TABLE IF EXISTS `proplancategory`;
 CREATE TABLE `proplancategory` (
   `PPCCode` varchar(5) NOT NULL,
   `PPCName` varchar(30) NOT NULL,
+  `PPCInUse` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`PPCCode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of proplancategory
 -- ----------------------------
-INSERT INTO `proplancategory` VALUES ('A11', '自然科学基金重大项目');
-INSERT INTO `proplancategory` VALUES ('A12', '自然科学基金一般项目');
-INSERT INTO `proplancategory` VALUES ('A13', '自然科学基金青年人才培养专项资金项目');
-INSERT INTO `proplancategory` VALUES ('C01', '重大科技专项（优先主题）重大工业项目');
-INSERT INTO `proplancategory` VALUES ('C02', '重大科技专项（优先主题）重大农业项目');
-INSERT INTO `proplancategory` VALUES ('C03', '重大科技专项（优先主题）重大社会发展项目');
-INSERT INTO `proplancategory` VALUES ('C04', '重大科技专项（优先主题）重大国际科技合作合作研究项目');
-INSERT INTO `proplancategory` VALUES ('C06', '重大科技专项（优先主题）重大国内合作成果转化项目');
-INSERT INTO `proplancategory` VALUES ('C07', '重大科技专项（优先主题）重大”863”科技成果对接项目');
-INSERT INTO `proplancategory` VALUES ('C11', '重大科技专项（优先主题）重点工业项目');
-INSERT INTO `proplancategory` VALUES ('C12', '重大科技专项（优先主题）重点农业项目');
-INSERT INTO `proplancategory` VALUES ('C13', '重大科技专项（优先主题）重点社会发展项目');
-INSERT INTO `proplancategory` VALUES ('C14', '重大科技专项（优先主题）重点国际科技合作合作研究项目');
-INSERT INTO `proplancategory` VALUES ('C15', '重大软科学项目');
-INSERT INTO `proplancategory` VALUES ('C16', '重大科技专项（优先主题）重点国内合作成果转化项目');
-INSERT INTO `proplancategory` VALUES ('C17', '重大科技专项（优先主题）重点”863”科技成果对接项目');
-INSERT INTO `proplancategory` VALUES ('C21', '公益技术应用研究工业项目');
-INSERT INTO `proplancategory` VALUES ('C22', '公益技术应用研究农业项目');
-INSERT INTO `proplancategory` VALUES ('C23', '公益技术应用研究社会发展项目');
-INSERT INTO `proplancategory` VALUES ('C24', '公益技术应用研究科技合作项目');
-INSERT INTO `proplancategory` VALUES ('C25', '重点软科学项目');
-INSERT INTO `proplancategory` VALUES ('C35', '一般软科学项目');
-INSERT INTO `proplancategory` VALUES ('C80', '自主知识产权成果转化项目');
-INSERT INTO `proplancategory` VALUES ('D20', '火炬计划');
-INSERT INTO `proplancategory` VALUES ('D30', '星火计划');
-INSERT INTO `proplancategory` VALUES ('D40', '科技型中小企业技术创新项目');
-INSERT INTO `proplancategory` VALUES ('D60', '新产品计划');
-INSERT INTO `proplancategory` VALUES ('D70', '农业科技成果转化项目');
-INSERT INTO `proplancategory` VALUES ('D80', '国际科技合作与引进消化吸收再创新重点项目');
-INSERT INTO `proplancategory` VALUES ('E10', '重点实验室和中试基地');
-INSERT INTO `proplancategory` VALUES ('E20', '区域科技创新服务中心');
-INSERT INTO `proplancategory` VALUES ('E40', '高新技术企业研究开发中心');
-INSERT INTO `proplancategory` VALUES ('E60', '科技创新平台');
-INSERT INTO `proplancategory` VALUES ('E70', '重点科技企业孵化器');
-INSERT INTO `proplancategory` VALUES ('E80', '引进大院名校共建创新载体');
-INSERT INTO `proplancategory` VALUES ('F10', '省属科研院所专项科技条件建设项目');
-INSERT INTO `proplancategory` VALUES ('F20', '省属科研院所专项创新团队建设与人才培项目');
-INSERT INTO `proplancategory` VALUES ('F30', '省属科研院所专项公共科技服务项目');
-INSERT INTO `proplancategory` VALUES ('G10', '科技特派员和科技扶贫');
-INSERT INTO `proplancategory` VALUES ('G20', '网上技术市场重点签约项目');
-INSERT INTO `proplancategory` VALUES ('G30', '长三角科技合作项目');
-INSERT INTO `proplancategory` VALUES ('G40', '院士基金');
-INSERT INTO `proplancategory` VALUES ('G90', '其他');
-INSERT INTO `proplancategory` VALUES ('R10', '钱江人才');
-INSERT INTO `proplancategory` VALUES ('R20', '科研院所优秀青年科技人才资助计划');
-INSERT INTO `proplancategory` VALUES ('R40', '新苗人才');
+INSERT INTO `proplancategory` VALUES ('A11', '自然科学基金重大项目', '1');
+INSERT INTO `proplancategory` VALUES ('A12', '自然科学基金一般项目', '1');
+INSERT INTO `proplancategory` VALUES ('A13', '自然科学基金青年人才培养专项资金项目', '1');
+INSERT INTO `proplancategory` VALUES ('C01', '重大科技专项（优先主题）重大工业项目', '1');
+INSERT INTO `proplancategory` VALUES ('C02', '重大科技专项（优先主题）重大农业项目', '1');
+INSERT INTO `proplancategory` VALUES ('C03', '重大科技专项（优先主题）重大社会发展项目', '1');
+INSERT INTO `proplancategory` VALUES ('C04', '重大科技专项（优先主题）重大国际科技合作合作研究项目', '1');
+INSERT INTO `proplancategory` VALUES ('C06', '重大科技专项（优先主题）重大国内合作成果转化项目', '1');
+INSERT INTO `proplancategory` VALUES ('C07', '重大科技专项（优先主题）重大”863”科技成果对接项目', '1');
+INSERT INTO `proplancategory` VALUES ('C11', '重大科技专项（优先主题）重点工业项目', '1');
+INSERT INTO `proplancategory` VALUES ('C12', '重大科技专项（优先主题）重点农业项目', '1');
+INSERT INTO `proplancategory` VALUES ('C13', '重大科技专项（优先主题）重点社会发展项目', '1');
+INSERT INTO `proplancategory` VALUES ('C14', '重大科技专项（优先主题）重点国际科技合作合作研究项目', '1');
+INSERT INTO `proplancategory` VALUES ('C15', '重大软科学项目', '1');
+INSERT INTO `proplancategory` VALUES ('C16', '重大科技专项（优先主题）重点国内合作成果转化项目', '1');
+INSERT INTO `proplancategory` VALUES ('C17', '重大科技专项（优先主题）重点”863”科技成果对接项目', '1');
+INSERT INTO `proplancategory` VALUES ('C21', '公益技术应用研究工业项目', '1');
+INSERT INTO `proplancategory` VALUES ('C22', '公益技术应用研究农业项目', '1');
+INSERT INTO `proplancategory` VALUES ('C23', '公益技术应用研究社会发展项目', '1');
+INSERT INTO `proplancategory` VALUES ('C24', '公益技术应用研究科技合作项目', '1');
+INSERT INTO `proplancategory` VALUES ('C25', '重点软科学项目', '1');
+INSERT INTO `proplancategory` VALUES ('C35', '一般软科学项目', '1');
+INSERT INTO `proplancategory` VALUES ('C80', '自主知识产权成果转化项目', '1');
+INSERT INTO `proplancategory` VALUES ('D20', '火炬计划', '1');
+INSERT INTO `proplancategory` VALUES ('D30', '星火计划', '1');
+INSERT INTO `proplancategory` VALUES ('D40', '科技型中小企业技术创新项目', '1');
+INSERT INTO `proplancategory` VALUES ('D60', '新产品计划', '1');
+INSERT INTO `proplancategory` VALUES ('D70', '农业科技成果转化项目', '1');
+INSERT INTO `proplancategory` VALUES ('D80', '国际科技合作与引进消化吸收再创新重点项目', '1');
+INSERT INTO `proplancategory` VALUES ('E10', '重点实验室和中试基地', '1');
+INSERT INTO `proplancategory` VALUES ('E20', '区域科技创新服务中心', '1');
+INSERT INTO `proplancategory` VALUES ('E40', '高新技术企业研究开发中心', '1');
+INSERT INTO `proplancategory` VALUES ('E60', '科技创新平台', '1');
+INSERT INTO `proplancategory` VALUES ('E70', '重点科技企业孵化器', '1');
+INSERT INTO `proplancategory` VALUES ('E80', '引进大院名校共建创新载体', '1');
+INSERT INTO `proplancategory` VALUES ('F10', '省属科研院所专项科技条件建设项目', '1');
+INSERT INTO `proplancategory` VALUES ('F20', '省属科研院所专项创新团队建设与人才培项目', '1');
+INSERT INTO `proplancategory` VALUES ('F30', '省属科研院所专项公共科技服务项目', '1');
+INSERT INTO `proplancategory` VALUES ('G10', '科技特派员和科技扶贫', '1');
+INSERT INTO `proplancategory` VALUES ('G20', '网上技术市场重点签约项目', '1');
+INSERT INTO `proplancategory` VALUES ('G30', '长三角科技合作项目', '1');
+INSERT INTO `proplancategory` VALUES ('G40', '院士基金', '1');
+INSERT INTO `proplancategory` VALUES ('G90', '其他', '1');
+INSERT INTO `proplancategory` VALUES ('R10', '钱江人才', '1');
+INSERT INTO `proplancategory` VALUES ('R20', '科研院所优秀青年科技人才资助计划', '1');
+INSERT INTO `proplancategory` VALUES ('R40', '新苗人才', '1');
 
 -- ----------------------------
 -- Table structure for `protecres`
@@ -329,15 +359,16 @@ DROP TABLE IF EXISTS `protecres`;
 CREATE TABLE `protecres` (
   `PTRCode` varchar(5) NOT NULL,
   `PTRName` varchar(20) NOT NULL,
+  `PTRInUse` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`PTRCode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of protecres
 -- ----------------------------
-INSERT INTO `protecres` VALUES ('1', '自主开发');
-INSERT INTO `protecres` VALUES ('2', '产学研合作开发');
-INSERT INTO `protecres` VALUES ('3', '引进技术本企业消化创新');
+INSERT INTO `protecres` VALUES ('1', '自主开发', '1');
+INSERT INTO `protecres` VALUES ('2', '产学研合作开发', '1');
+INSERT INTO `protecres` VALUES ('3', '引进技术本企业消化创新', '1');
 
 -- ----------------------------
 -- Table structure for `teacher`
@@ -347,7 +378,10 @@ CREATE TABLE `teacher` (
   `TId` varchar(10) NOT NULL,
   `TName` varchar(20) NOT NULL,
   `TPosition` varchar(10) NOT NULL,
+  `TPhone` varchar(15) NOT NULL,
   `TCardId` varchar(20) NOT NULL,
+  `TCreateDate` datetime NOT NULL,
+  `TStopDate` datetime DEFAULT NULL,
   PRIMARY KEY (`TId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -362,73 +396,74 @@ DROP TABLE IF EXISTS `tecmngarea`;
 CREATE TABLE `tecmngarea` (
   `TMACode` varchar(5) NOT NULL,
   `TMAName` varchar(20) NOT NULL,
+  `TMAInUse` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`TMACode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tecmngarea
 -- ----------------------------
-INSERT INTO `tecmngarea` VALUES ('A1', '电子信息');
-INSERT INTO `tecmngarea` VALUES ('A11', '网络');
-INSERT INTO `tecmngarea` VALUES ('A12', '通信');
-INSERT INTO `tecmngarea` VALUES ('A13', '计算机硬件');
-INSERT INTO `tecmngarea` VALUES ('A14', '计算机软件');
-INSERT INTO `tecmngarea` VALUES ('A15', '自动化控制');
-INSERT INTO `tecmngarea` VALUES ('A16', '电子及其他');
-INSERT INTO `tecmngarea` VALUES ('A2', '能源');
-INSERT INTO `tecmngarea` VALUES ('A21', '新能源');
-INSERT INTO `tecmngarea` VALUES ('A22', '节能技术及其他');
-INSERT INTO `tecmngarea` VALUES ('A30', '交通');
-INSERT INTO `tecmngarea` VALUES ('A4', '材料');
-INSERT INTO `tecmngarea` VALUES ('A41', '无机材料');
-INSERT INTO `tecmngarea` VALUES ('A42', '有机材料');
-INSERT INTO `tecmngarea` VALUES ('A43', '金属材料');
-INSERT INTO `tecmngarea` VALUES ('A5', '化工');
-INSERT INTO `tecmngarea` VALUES ('A51', '无机化工');
-INSERT INTO `tecmngarea` VALUES ('A52', '有机化工');
-INSERT INTO `tecmngarea` VALUES ('A53', '精细化工');
-INSERT INTO `tecmngarea` VALUES ('A54', '催化');
-INSERT INTO `tecmngarea` VALUES ('A55', '化学工程与工艺');
-INSERT INTO `tecmngarea` VALUES ('A60', '纺织');
-INSERT INTO `tecmngarea` VALUES ('A70', '轻工');
-INSERT INTO `tecmngarea` VALUES ('A8', '建材与城建');
-INSERT INTO `tecmngarea` VALUES ('A80', '建材');
-INSERT INTO `tecmngarea` VALUES ('A85', '城建');
-INSERT INTO `tecmngarea` VALUES ('A9', '机电');
-INSERT INTO `tecmngarea` VALUES ('A91', '机电一体化');
-INSERT INTO `tecmngarea` VALUES ('A92', '通用机械与设备');
-INSERT INTO `tecmngarea` VALUES ('A93', '化工机械');
-INSERT INTO `tecmngarea` VALUES ('A94', '仪器仪表');
-INSERT INTO `tecmngarea` VALUES ('B1', '种植业');
-INSERT INTO `tecmngarea` VALUES ('B11', '粮油作物');
-INSERT INTO `tecmngarea` VALUES ('B12', '经济作物');
-INSERT INTO `tecmngarea` VALUES ('B2', '渔业');
-INSERT INTO `tecmngarea` VALUES ('B21', '海洋渔业');
-INSERT INTO `tecmngarea` VALUES ('B22', '淡水渔业');
-INSERT INTO `tecmngarea` VALUES ('B3', '林特业');
-INSERT INTO `tecmngarea` VALUES ('B31', '林业');
-INSERT INTO `tecmngarea` VALUES ('B32', '林特产');
-INSERT INTO `tecmngarea` VALUES ('B4', '畜牧业');
-INSERT INTO `tecmngarea` VALUES ('B41', '畜禽养殖');
-INSERT INTO `tecmngarea` VALUES ('B49', '其他畜牧业');
-INSERT INTO `tecmngarea` VALUES ('B5', '农产品加工');
-INSERT INTO `tecmngarea` VALUES ('B51', '动植物加工');
-INSERT INTO `tecmngarea` VALUES ('B52', '林特产加工及其他');
-INSERT INTO `tecmngarea` VALUES ('C1', '医药卫生');
-INSERT INTO `tecmngarea` VALUES ('C11', '外科');
-INSERT INTO `tecmngarea` VALUES ('C12', '内科');
-INSERT INTO `tecmngarea` VALUES ('C13', '综合医学');
-INSERT INTO `tecmngarea` VALUES ('C14', '生物制药');
-INSERT INTO `tecmngarea` VALUES ('C15', '化学制药');
-INSERT INTO `tecmngarea` VALUES ('C16', '中医药');
-INSERT INTO `tecmngarea` VALUES ('C2', '资源与环境');
-INSERT INTO `tecmngarea` VALUES ('C21', '资源开发利用');
-INSERT INTO `tecmngarea` VALUES ('C22', '环保技术');
-INSERT INTO `tecmngarea` VALUES ('C23', '环保装备');
-INSERT INTO `tecmngarea` VALUES ('C30', '社会事业');
-INSERT INTO `tecmngarea` VALUES ('C40', '服务业');
-INSERT INTO `tecmngarea` VALUES ('C5', '海洋');
-INSERT INTO `tecmngarea` VALUES ('C51', '养殖与捕捞');
-INSERT INTO `tecmngarea` VALUES ('C52', '临港工业');
-INSERT INTO `tecmngarea` VALUES ('C60', '公共安全与防灾减灾');
-INSERT INTO `tecmngarea` VALUES ('D10', '软科学');
+INSERT INTO `tecmngarea` VALUES ('A1', '电子信息', '1');
+INSERT INTO `tecmngarea` VALUES ('A11', '网络', '1');
+INSERT INTO `tecmngarea` VALUES ('A12', '通信', '1');
+INSERT INTO `tecmngarea` VALUES ('A13', '计算机硬件', '1');
+INSERT INTO `tecmngarea` VALUES ('A14', '计算机软件', '1');
+INSERT INTO `tecmngarea` VALUES ('A15', '自动化控制', '1');
+INSERT INTO `tecmngarea` VALUES ('A16', '电子及其他', '1');
+INSERT INTO `tecmngarea` VALUES ('A2', '能源', '1');
+INSERT INTO `tecmngarea` VALUES ('A21', '新能源', '1');
+INSERT INTO `tecmngarea` VALUES ('A22', '节能技术及其他', '1');
+INSERT INTO `tecmngarea` VALUES ('A30', '交通', '1');
+INSERT INTO `tecmngarea` VALUES ('A4', '材料', '1');
+INSERT INTO `tecmngarea` VALUES ('A41', '无机材料', '1');
+INSERT INTO `tecmngarea` VALUES ('A42', '有机材料', '1');
+INSERT INTO `tecmngarea` VALUES ('A43', '金属材料', '1');
+INSERT INTO `tecmngarea` VALUES ('A5', '化工', '1');
+INSERT INTO `tecmngarea` VALUES ('A51', '无机化工', '1');
+INSERT INTO `tecmngarea` VALUES ('A52', '有机化工', '1');
+INSERT INTO `tecmngarea` VALUES ('A53', '精细化工', '1');
+INSERT INTO `tecmngarea` VALUES ('A54', '催化', '1');
+INSERT INTO `tecmngarea` VALUES ('A55', '化学工程与工艺', '1');
+INSERT INTO `tecmngarea` VALUES ('A60', '纺织', '1');
+INSERT INTO `tecmngarea` VALUES ('A70', '轻工', '1');
+INSERT INTO `tecmngarea` VALUES ('A8', '建材与城建', '1');
+INSERT INTO `tecmngarea` VALUES ('A80', '建材', '1');
+INSERT INTO `tecmngarea` VALUES ('A85', '城建', '1');
+INSERT INTO `tecmngarea` VALUES ('A9', '机电', '1');
+INSERT INTO `tecmngarea` VALUES ('A91', '机电一体化', '1');
+INSERT INTO `tecmngarea` VALUES ('A92', '通用机械与设备', '1');
+INSERT INTO `tecmngarea` VALUES ('A93', '化工机械', '1');
+INSERT INTO `tecmngarea` VALUES ('A94', '仪器仪表', '1');
+INSERT INTO `tecmngarea` VALUES ('B1', '种植业', '1');
+INSERT INTO `tecmngarea` VALUES ('B11', '粮油作物', '1');
+INSERT INTO `tecmngarea` VALUES ('B12', '经济作物', '1');
+INSERT INTO `tecmngarea` VALUES ('B2', '渔业', '1');
+INSERT INTO `tecmngarea` VALUES ('B21', '海洋渔业', '1');
+INSERT INTO `tecmngarea` VALUES ('B22', '淡水渔业', '1');
+INSERT INTO `tecmngarea` VALUES ('B3', '林特业', '1');
+INSERT INTO `tecmngarea` VALUES ('B31', '林业', '1');
+INSERT INTO `tecmngarea` VALUES ('B32', '林特产', '1');
+INSERT INTO `tecmngarea` VALUES ('B4', '畜牧业', '1');
+INSERT INTO `tecmngarea` VALUES ('B41', '畜禽养殖', '1');
+INSERT INTO `tecmngarea` VALUES ('B49', '其他畜牧业', '1');
+INSERT INTO `tecmngarea` VALUES ('B5', '农产品加工', '1');
+INSERT INTO `tecmngarea` VALUES ('B51', '动植物加工', '1');
+INSERT INTO `tecmngarea` VALUES ('B52', '林特产加工及其他', '1');
+INSERT INTO `tecmngarea` VALUES ('C1', '医药卫生', '1');
+INSERT INTO `tecmngarea` VALUES ('C11', '外科', '1');
+INSERT INTO `tecmngarea` VALUES ('C12', '内科', '1');
+INSERT INTO `tecmngarea` VALUES ('C13', '综合医学', '1');
+INSERT INTO `tecmngarea` VALUES ('C14', '生物制药', '1');
+INSERT INTO `tecmngarea` VALUES ('C15', '化学制药', '1');
+INSERT INTO `tecmngarea` VALUES ('C16', '中医药', '1');
+INSERT INTO `tecmngarea` VALUES ('C2', '资源与环境', '1');
+INSERT INTO `tecmngarea` VALUES ('C21', '资源开发利用', '1');
+INSERT INTO `tecmngarea` VALUES ('C22', '环保技术', '1');
+INSERT INTO `tecmngarea` VALUES ('C23', '环保装备', '1');
+INSERT INTO `tecmngarea` VALUES ('C30', '社会事业', '1');
+INSERT INTO `tecmngarea` VALUES ('C40', '服务业', '1');
+INSERT INTO `tecmngarea` VALUES ('C5', '海洋', '1');
+INSERT INTO `tecmngarea` VALUES ('C51', '养殖与捕捞', '1');
+INSERT INTO `tecmngarea` VALUES ('C52', '临港工业', '1');
+INSERT INTO `tecmngarea` VALUES ('C60', '公共安全与防灾减灾', '1');
+INSERT INTO `tecmngarea` VALUES ('D10', '软科学', '1');
