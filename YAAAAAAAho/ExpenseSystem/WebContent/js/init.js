cb287f2565e265bb97d20c86f9b996e57a501b7c
RRichardLiuu <jiapeng0808@hotmail.com>
2017-07-11 09:50:03 +0800

    ui3

--- YAAAAAAAho/ExpenseSystem/WebContent/js/init.js
+++ YAAAAAAAho/ExpenseSystem/WebContent/js/init.js
@@ -10,5 +10,22 @@
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15 // Creates a dropdown of 15 years to control year
       });
+    
+    $('.chips').material_chip();
+    $('.chips-initial').material_chip({
+      data: [{
+        tag: '设备费',
+      }, {
+        tag: '材料费',
+      }, {
+        tag: '差旅费',
+      }],
+    });
+    $('.chips-placeholder').material_chip({
+      placeholder: 'Enter a tag',
+      secondaryPlaceholder: '+Tag',
+    });
+    
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
